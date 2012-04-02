f11e775e9755f2b802e959f7ebcde8fcc2c4560d
Rob Cakebread <cakebread@gmail.com>
2012-04-02 14:25:07 -0700

    Make basic Twilio endpoint

--- phox/views/main.py
+++ phox/views/main.py
@@ -1,7 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from twilio import twiml
+
 
 main = Blueprint('main', __name__)
 
 
 def index(name=None):
+    """Will contain login/status page"""
     return render_template('index.html', name=name)
+
+@main.route("/voice")
+def voice():
+    """Hello! Goodbye."""
+    from_number = request.args.get('From', None)
+    response = twiml.Response()
+    response.say("Thanks for calling. Goodbye.")
+    return str(response)
+
+@main.route("/sms")
+def sms():
+    """A basic echo responder"""
+    response = twiml.Response()
+    message = request.args.get('Body', None)
+    response.sms("Thanks for texting the following: '%s'\nGood bye." % message)
+    return str(response)
